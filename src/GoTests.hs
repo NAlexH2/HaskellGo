a64711554abd645e1d4afa9bf0d93028746c6900
NAlexH2 <nharris356087@gmail.com>
2023-03-05 10:56:00 -0800

    Just getting ready to make tests

---
+++ src/GoTests.hs
@@ -0,0 +1,18 @@
+module Gotests where
+
+import GoConsts
+import GoTypesData
+import GoWork
+import HaskellGo
+import Test.HUnit
+
+{-- 
+Big test, the game "played" itself to ensure stones were captured and score
+was updated and the newboard was produced.
+--}
+
+
+
+{--
+All other tests
+--}
