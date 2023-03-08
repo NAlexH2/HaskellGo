1f7983340f98618809b2b6f5c283f1a5b9c5c384
NAlexH2 <nharris356087@gmail.com>
2023-03-07 22:32:45 -0800

    added capped singles test

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,13 +3,13 @@ import Test.HUnit
 
 main :: IO ()
 main = do
-  -- _ <- runTestTT $ TestList [ testIdentifyUnits ]
+  -- _ <- runTestTT $ TestList [ testCappedSingles ]
   _ <- runTestTT $ TestList 
         [ 
           testCurrentPlayerStats, testBoardState, testStatsState,
           testEmptyBoard, testCurrentRow, testPreviousRow, testNextRow,
           testGetPosition, testRowLimit, testPosCalc, testLegalMove, 
           testUpdatePlayerPass, testGetPassCount, testRowStates, 
-          testIdentifyUnits
+          testIdentifyUnits, testCappedSingles
         ]
   return ()
