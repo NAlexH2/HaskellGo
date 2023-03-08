0c13a554d70c8cb5085f21c3d97ab26cbbef76c4
NAlexH2 <nharris356087@gmail.com>
2023-03-07 18:15:57 -0800

    Identify units test added

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,29 +1,15 @@
-import GoTests  (
-                  testCurrentPlayerStats,
-                  testBoardState, 
-                  testStatsState,
-                  testEmptyBoard,
-                  testCurrentRow,
-                  testPreviousRow,
-                  testNextRow,
-                  testGetPosition,
-                  testRowLimit,
-                  testLegalMove,
-                  testUpdatePlayerPass,
-                  testGetPassCount,
-                  testRowStates,
-                  testIdentifyUnits
-                )
+import GoTests
 import Test.HUnit
 
 main :: IO ()
 main = do
-  _ <- runTestTT $ TestList [ testIdentifyUnits ]
-  -- _ <- runTestTT $ TestList 
-  --       [ 
-  --         testCurrentPlayerStats, testBoardState, testStatsState,
-  --         testEmptyBoard, testCurrentRow, testPreviousRow, testNextRow,
-  --         testGetPosition, testRowLimit, testLegalMove, testUpdatePlayerPass,
-  --         testGetPassCount, testRowStates
-  --       ]
+  -- _ <- runTestTT $ TestList [ testIdentifyUnits ]
+  _ <- runTestTT $ TestList 
+        [ 
+          testCurrentPlayerStats, testBoardState, testStatsState,
+          testEmptyBoard, testCurrentRow, testPreviousRow, testNextRow,
+          testGetPosition, testRowLimit, testPosCalc, testLegalMove, 
+          testUpdatePlayerPass, testGetPassCount, testRowStates, 
+          testIdentifyUnits
+        ]
   return ()
