7f4331f6dc46ec61e77f77861ed86d8f1d3ea17e
NAlexH2 <nharris356087@gmail.com>
2023-03-08 19:33:23 -0800

    testing just captured units currently. WIP

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,13 +3,13 @@ import Test.HUnit
 
 main :: IO ()
 main = do
-  -- _ <- runTestTT $ TestList [ testCappedSingles ]
-  _ <- runTestTT $ TestList 
-        [ 
-          testCurrentPlayerStats, testBoardState, testStatsState,
-          testEmptyBoard, testCurrentRow, testPreviousRow, testNextRow,
-          testGetPosition, testRowLimit, testPosCalc, testLegalMove, 
-          testUpdatePlayerPass, testGetPassCount, testRowStates, 
-          testIdentifyUnits, testCappedSingles
-        ]
+  _ <- runTestTT $ TestList [ testCappedUnits ]
+  -- _ <- runTestTT $ TestList 
+        -- [ 
+          -- testCurrentPlayerStats, testBoardState, testStatsState,
+          -- testEmptyBoard, testCurrentRow, testPreviousRow, testNextRow,
+          -- testGetPosition, testRowLimit, testPosCalc, testLegalMove, 
+          -- testUpdatePlayerPass, testGetPassCount, testRowStates, 
+          -- testIdentifyUnits, testCappedSingles
+        -- ]
   return ()
