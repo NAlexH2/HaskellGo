86c5d78d95760206e714ab9f3af6cdfab3dc6350
NAlexH2 <nharris356087@gmail.com>
2023-03-13 21:15:40 -0700

    Cleaning up

--- src/HaskellGo.hs
+++ src/HaskellGo.hs
@@ -9,50 +9,71 @@ import GoCapture
 import GoTypesData
 import GoConsts
 import Text.Printf ( printf )
+import System.Exit (exitSuccess)
 
 
--- //TODO - Improve comment
--- Where it all starts. Recursively runs the game using a do statement.
+-- Where it all starts. Recursively runs the game using a do statement while
+-- performing the work and logic before and after the player enters their
+-- move.
 haskellGo :: GameState -> PlayerID -> IO ()
 haskellGo currentGame pID =
   do
     let board = boardState currentGame
     let stats = statsState currentGame
-    -- //HACK - Remove these in final version
-    -- print ((length board)-1)
-    -- print (board !! 80)
-    -- print (show (boardState currentGame))
+    let pID' = turnToggle pID
+
     printf "Type 'quit' anytime to end the game or 'pass' to skip your turn.\n"
     printf "To make your move, simply type an x and y that is on the grid. "
     printf "Like so:\n'x y: 9 9'\n\n"
-    let pID' = turnToggle pID
-    displayState bdSz currentGame
+    displayState bdSz stats board
     printf "It is player %s's turn...\n" (currentPlayer pID')
     putStr "x y: "
     move <- getCoordinates
-    -- //HACK - remove in final version
-    -- print (show (statsState currentGame))
-    -- uncurry (printf "\n\n\n(%d,%d) %d\n\n\n") move (posCalc move)
-    let legality = legalMove bdSz board move
-    if legality > 1 then do
-      _ <- clearScreen
-      -- //HACK - Remove these in final version
-      -- uncurry (printf "\n\n\n(%d,%d)\n\n\n") move
-      printf "\n**ERROR** - %s\n" (errorBadMove legality)
-      let pID'' = turnToggle pID'
-      haskellGo currentGame pID''
+    let stats' = updatePlayerPass pID' move stats
+
+    if (getPassCount pID' stats' >= 2) || move == quit 
+      then endGame bdSz stats' board
+
     else do
-      -- //TODO see if player passed 2 times, end game if so.
-      let stats' = updatePlayerPass pID' move stats
-      -- //TODO check pass count here
-      let board' = makeBoard bdSz pID' board (posCalc move bdSz) []
-      let captured = capturedStones bdSz (turnToggle pID') board
-      let newStats = updateStats pID' stats' captured
-      let newBd = makeBoard bdSz pID' board' (posCalc move bdSz) captured
-      let currentGame' = updateGame newStats newBd
-      haskellGo currentGame' pID'
+      let legality = legalMove bdSz board move
+      if legality > 1 then do
+        _ <- clearScreen
+        printf "\n**ERROR** - %s\n" (errorBadMove legality)
+        let pID'' = turnToggle pID'
+        haskellGo currentGame pID''
+
+      else do
+        _ <- clearScreen
+        let movedBoard = makeBoard bdSz pID' board (posCalc move bdSz) []
+        let captured = capturedStones bdSz (turnToggle pID') movedBoard
+        let newStats = updateStats pID' stats' captured
+        let newBd = makeBoard bdSz pID' movedBoard (posCalc move bdSz) captured
+        let updatedGame = updateGame newStats newBd
+        haskellGo updatedGame pID'
   where
     bdSz = boardSize
+
+
+-- Runs the endgame steps to display the final board and examine the score
+endGame :: Int -> [PlayerStats] -> Board -> IO()
+endGame bdSz stats board =
+  do 
+    _ <- clearScreen
+    printf "\n***The game is OVER!***\n"
+    displayState bdSz stats board
+    winner stats
+    exitSuccess
+
+
+-- Displays the winner based on current score
+winner :: [PlayerStats] -> IO()
+winner stats 
+  | sB > sW = printf "\n!!!!BLACK WINS!!!!\n\n"
+  | sB < sW = printf "\n!!!!WHITE WINS!!!!\n\n"
+  | otherwise = printf "\n***THE GAME IS TIED***\n\n"
+  where
+    sB = getPlayerScore PB stats
+    sW = getPlayerScore PW stats
 
 
 -- Error handling for the game. Allows user to correct mistakes
@@ -64,14 +85,14 @@ errorBadMove e  | e == 2      = "Received invalid input from the user."
 
 
 -- Displays the current state of the game. 
-displayState :: Int -> GameState -> IO ()
-displayState bdSz gameState =
+displayState :: Int -> [PlayerStats] -> Board -> IO ()
+displayState bdSz stats board =
   do
     displayTopRows
-    displayEachRow bdSz row (boardState gameState)
-    displayScore (statsState gameState)
+    displayEachRow bdSz row board
+    displayScore stats
       where
-          row = 1
+          row = 0
 
 
 -- Visualization of the board to the user for x y coordinates to be entered
@@ -82,9 +103,9 @@ displayTopRows = printf "  x  1 2 3 4 5 6 7 8 9\ny   __________________"
 -- Displays most up to date state of the board as to be human readable.
 displayEachRow :: Int -> Int -> Board -> IO ()
 displayEachRow bdSz row board =
-    if row < bdSz+1 then
+    if row /= bdSz then
       do
-        let x = show row ++ " | " ++ rowStates (rowLimit bdSz row) board
+        let x = show (row+1) ++ " | " ++ rowStates (rowLimit bdSz row) board
         printf "\n%s" x
         displayEachRow bdSz (row+1) board
     else printf "\n"
@@ -95,8 +116,8 @@ displayScore :: [PlayerStats] -> IO ()
 displayScore stats =
   do
     printf "---------------------------------\n"
-    printf "      Score -- b: %d -- w: %d\n\n" sB sW
-    printf "Pass Counts -- b: %d -- w: %d\n\n" pB pW
+    printf "     Score -- b: %d -- w: %d\n" sB sW
+    printf "Pass Count -- b: %d -- w: %d\n\n" pB pW
   where
     sB = getPlayerScore PB stats
     sW = getPlayerScore PW stats
@@ -105,13 +126,10 @@ displayScore stats =
 
 
 {-- /*TODO
-  [ ] Check pass counter and stop the game if either player has a count of
-      2.
-  [ ] Tabulate the score when the game ends either with pass = 2 or quit
-  [ ] Write a big test to check all this?
 
-  
   *** COMPLETED TASKS ***
+  [x] Check pass counter and stop the game if either player has a count of 2.
+  [x] Tabulate the score when the game ends either with pass = 2 or quit.
   [x] Display stats (including pass count)
   [x] Must update the board with new move first, check the board for captures,
       then update the stats/board/etc blah blah
