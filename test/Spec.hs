6cab636f388f1cde76820cbf7cecf43abf26980c
NAlexH2 <nharris356087@gmail.com>
2023-03-13 17:24:00 -0700

    full test suite enabled

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,14 +3,13 @@ import Test.HUnit ( Test(TestList), runTestTT )
 
 main :: IO ()
 main = do
-  _ <- runTestTT $ TestList [ testUpdateStats ]
-  -- _ <- runTestTT $ TestList 
-  --       [ 
-  --         testCurrentPlayerStats, testBoardState, testStatsState,
-  --         testEmptyBoard, testCurrentRow, testPreviousRow, testNextRow,
-  --         testGetNext, testGetPosition, testRowLimit, testPosCalc,
-  --         testLegalMove, testUpdatePlayerPass, testGetPassCount,  
-  --         testRowStates, testIdentifyUnits, testCappedSingles,
-  --         testCapturedStones
-  --       ]
+  -- _ <- runTestTT $ TestList [  ]
+  _ <- runTestTT $ TestList 
+        [ 
+          testCurrentPlayerStats, testBoardState, testStatsState,
+          testEmptyBoard, testCurrentRow, testPreviousRow, testNextRow,
+          testGetPosition, testRowLimit, testPosCalc, testLegalMove, 
+          testUpdatePlayerPass, testGetPassCount, testRowStates, 
+          testIdentifyUnits, testCappedSingles, testCapturedStones
+        ]
   return ()
